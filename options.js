8418184b5db8d91c6f9db240b22f78f64bbbd1bd
juancarlos-ruiz <juancarlos.ruiz@adevinta.com>
2020-09-22 17:52:25 +0200

    First draft version

--- options.js
+++ options.js
@@ -1,28 +1,63 @@
-"use strict";
+const INPUT_FORM = [
+  {
+    label: "Your JIRA domain",
+    name: "basePath",
+    placeholder: "https://your-jira.atlassian.net/",
+    type: "url",
+  },
+  {
+    label: "Your company name",
+    name: "companyName",
+  },
+  {
+    label: "Company logo URL",
+    name: "companyLogo",
+    placeholder: "https://...../logo.png",
+    type: "url",
+  },
+  {
+    label: "Board ID",
+    name: "boardId",
+    placeholder: "Number in url ...RapidBoard.jspa?rapidView=4471",
+    type: "number",
+  },
+];
+
+const INPUT_KEYS = INPUT_FORM.map(({ name }) => name);
 
 const configForm = document.getElementById("configForm");
-const jiraBasePath = document.getElementById("jiraBasePath");
-const jiraCompanyName = document.getElementById("jiraCompanyName");
-const jiraCompanyLogo = document.getElementById("jiraCompanyLogo");
+const messages = document.getElementById("messages");
+const form = document.getElementById("form");
 
-chrome.storage.sync.get(
-  ["basePath", "companyName", "companyLogo"],
-  ({ basePath, companyName, companyLogo }) => {
-    jiraBasePath.setAttribute("value", basePath || "");
-    jiraCompanyName.setAttribute("value", companyName || "");
-    jiraCompanyLogo.setAttribute("value", companyLogo || "");
-  }
-);
+const genForm = (values) =>
+  (form.innerHTML = INPUT_FORM.map(
+    ({ name, label, type, placeholder }) => `
+        <div class="form-control">
+          <label for="${name}">${label}</label>
+          <input
+            value="${values?.[name] || ""}"
+            type="${type || "text"}"
+            name="${name}"
+            id="${name}"
+            placeholder="${placeholder || ""}"
+          />
+        </div> 
+      `
+  ));
+
+chrome.storage.sync.get(INPUT_KEYS, (values) => genForm(values));
 
 configForm.onsubmit = (event) => {
   event.preventDefault();
-  const jiraBasePathValue = jiraBasePath?.value;
-  const jiraCompanyNameValue = jiraCompanyName?.value;
-  const jiraCompanyLogoValue = jiraCompanyLogo?.value;
+  messages.innerHTML = "";
 
-  chrome.storage.sync.set({
-    basePath: jiraBasePathValue,
-    companyName: jiraCompanyNameValue,
-    companyLogo: jiraCompanyLogoValue,
-  });
+  chrome.storage.sync.set(
+    {
+      basePath: document.getElementById("basePath")?.value,
+      companyName: document.getElementById("companyName")?.value,
+      companyLogo: document.getElementById("companyLogo")?.value,
+      boardId: document.getElementById("boardId")?.value,
+    },
+    () => (messages.innerHTML = "<span>Config updated!</span>")
+  );
 };
--- popup.js
+++ popup.js
@@ -1,6 +1,10 @@
 "use strict";
 
 const expandOpenTasks = document.getElementById("expandOpenTasks");
+const expandClosedTickets = document.getElementById("expandClosedTickets");
+const expandAllTickets = document.getElementById("expandAllTickets");
+const collapseAllTickets = document.getElementById("collapseAllTickets");
+
 const newTask = document.getElementById("newTask");
 const viewSprint = document.getElementById("viewSprint");
 const viewBacklog = document.getElementById("viewBacklog");
@@ -14,15 +18,30 @@ const code = `document.querySelectorAll('.ghx-swimlane').forEach(item =>
     : item.classList.add('ghx-closed')
 )`;
 
+const expandClosedTicketsCode = `document.querySelectorAll('.ghx-swimlane').forEach(item =>
+  item.querySelectorAll('.ghx-swimlane-header.ghx-done')?.length === 0
+    ? item.classList.add('ghx-closed')
+    : item.classList.remove('ghx-closed')
+)`;
+
+const expandAllTicketsCode = `document.querySelectorAll('.ghx-swimlane').forEach(
+  item => item.classList.remove('ghx-closed')
+)`;
+
+const collapseAllTicketsCode = `document.querySelectorAll('.ghx-swimlane').forEach(
+  item => item.classList.add('ghx-closed')
+)`;
+
 chrome.storage.sync.get(
-  ["basePath", "companyName", "companyLogo"],
-  ({ basePath, companyName, companyLogo }) => {
+  ["basePath", "companyName", "companyLogo", "boardId"],
+  ({ basePath, companyName, companyLogo, boardId }) => {
     companyInfo.innerHTML = `
       ${companyLogo ? `<img src="${companyLogo}" alt="${companyName}" />` : ""}
       <h1>${companyName || "Jira utils extension"}</h1>
       <span>${basePath || "Needed config yout jira domain"}</span>
     `;
 
+    /*
     document.getElementById("config").onclick = () => {
       document.getElementById("tabs").classList.toggle("is-active");
       document.getElementById("overlay").classList.toggle("is-active");
@@ -31,24 +50,24 @@ chrome.storage.sync.get(
     document.getElementById("overlay").onclick = () => {
       document.getElementById("tabs").classList.toggle("is-active");
       document.getElementById("overlay").classList.toggle("is-active");
-    };
+    };*/
 
     viewSprint.onclick = () => {
       chrome.tabs.create({
-        url: `${basePath}/secure/RapidBoard.jspa?rapidView=4471`,
+        url: `${basePath}/secure/RapidBoard.jspa?rapidView=${boardId}`,
       });
     };
 
     viewBacklog.onclick = () => {
       chrome.tabs.create({
-        url: `${basePath}/secure/RapidBoard.jspa?rapidView=4471&view=planning`,
+        url: `${basePath}/secure/RapidBoard.jspa?rapidView=${boardId}&view=planning`,
       });
     };
 
     viewTask.onsubmit = () => {
       const id = viewTaskId.value;
       chrome.tabs.create({
-        url: `${basePath}/browse/${id}`,
+        url: `${basePath}/secure/QuickSearch.jspa?searchString=${id}`,
       });
     };
 
@@ -67,6 +86,30 @@ chrome.storage.sync.get(
       chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.tabs.executeScript(tabs[0].id, {
           code,
+        });
+      });
+    };
+
+    expandClosedTickets.onclick = () => {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.executeScript(tabs[0].id, {
+          code: expandClosedTicketsCode,
+        });
+      });
+    };
+
+    expandAllTickets.onclick = () => {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.executeScript(tabs[0].id, {
+          code: expandAllTicketsCode,
+        });
+      });
+    };
+
+    collapseAllTickets.onclick = () => {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.executeScript(tabs[0].id, {
+          code: collapseAllTicketsCode,
         });
       });
     };
